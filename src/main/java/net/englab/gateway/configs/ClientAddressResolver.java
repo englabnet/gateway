3eb9576bfcd5b36ff3956e214df897c087f3c0b1
Nikita Kuchur <nikitakuchur@gmail.com>
2023-11-06 22:25:17 +0100

    rate limiter fix

--- src/main/java/net/englab/gateway/configs/ClientAddressResolver.java
+++ src/main/java/net/englab/gateway/configs/ClientAddressResolver.java
@@ -1,22 +1,19 @@
 package net.englab.gateway.configs;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.support.ipresolver.XForwardedRemoteAddressResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.Optional;
 
 @Component
 public class ClientAddressResolver implements KeyResolver {
     @Override
     public Mono<String> resolve(ServerWebExchange exchange) {
-        return Optional.ofNullable(exchange.getRequest().getRemoteAddress())
-                .map(InetSocketAddress::getAddress)
-                .map(InetAddress::getHostAddress)
-                .map(Mono::just)
-                .orElse(Mono.empty());
+        XForwardedRemoteAddressResolver resolver = XForwardedRemoteAddressResolver.maxTrustedIndex(1);
+        InetSocketAddress inetSocketAddress = resolver.resolve(exchange);
+        return Mono.just(inetSocketAddress.getAddress().getHostAddress());
     }
 }
